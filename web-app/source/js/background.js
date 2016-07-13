68f10d26ddee1737b1fdaf38e7dba86dd0736a88
Carey Williams <careyjwilliams@gmail.com>
2016-07-13 20:14:03 +0100

    added min/max window size bounds for Chrome

--- web-app/source/js/background.js
+++ web-app/source/js/background.js
@@ -7,6 +7,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('index.html', {
 		'outerBounds' : {
+			'minWidth': 800, 
+			'minHeight': 600,
 			'width': 800,
 			'height': 700
 		}
